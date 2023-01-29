a58de7ee9a0f5ebd5c9e2e63811a664ba15059f0
maynard-olegario <maynard-olegario@users.noreply.github.com>
2023-01-28 20:02:27 -0800

    fix infinite scrolling

--- scripts/linkedin.py
+++ scripts/linkedin.py
@@ -2,22 +2,22 @@ import time
 
 
 def scroll_to_end(browser):
-    scroll_pause_time = 1
+    time.sleep(2)  # Allow 2 seconds for the web page to open
+    scroll_pause_time = 2
     screen_height = browser.execute_script("return window.screen.height;")
     i = 1
     while True:
         # scroll one screen height each time
-        browser.execute_script(
-            "window.scrollTo(0, {screen_height}*{i});".format(
-                screen_height=screen_height, i=i
-            )
-        )
-        i += 1
-        time.sleep(scroll_pause_time)
+        browser.execute_script("window.scrollBy(0, window.innerHeight);")
 
-        see_more_button = browser.find_element_by_xpath("//button[@aria-label='See more jobs']")
+        see_more_button = browser.find_element_by_xpath(
+            "//button[@aria-label='See more jobs']"
+        )
         if see_more_button.is_displayed():
             see_more_button.click()
+
+        i += 1
+        time.sleep(scroll_pause_time)
 
         # update scroll height each time after scrolled, as the scroll height can change after we scrolled the page
         scroll_height = browser.execute_script("return document.body.scrollHeight;")
@@ -38,7 +38,7 @@ def filter_job(job):
     if len(blacklisted_words_in_title) > 0:
         print(f"FILTERING - Word(s) found in title: {blacklisted_words_in_title}")
         return None
-    
+
     for r in required:
         if r in job["description"].lower():
             job["description"] = "PYTHON IN DESCRIPTION --- " + job["description"]
